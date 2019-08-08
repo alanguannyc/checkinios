43b948326b92d70ce8d4dea4702b1c596633ab15
Alan <alanseau@gmail.com>
2019-08-08 12:55:11 -0400

    Initial Commit

---
+++ CheckinApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckinApp
+//
+//  Created by Alan on 8/8/19.
+//  Copyright © 2019 Shangguan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
